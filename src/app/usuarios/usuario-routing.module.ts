6b335e16aee3bdfa3d515f30c62e40bd724dc23b
osmar-asus <osmarfilho6@gmail.com>
2020-01-10 07:29:16 -0300

    Navegaçao para formulario de usuario

--- src/app/usuarios/usuario-routing.module.ts
+++ src/app/usuarios/usuario-routing.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { UsuarioComponent } from './usuario/usuario.component';
 import { UsuarioFormComponent } from './usuario-form/usuario-form.component';
 
 const usuarioRoutes: Routes = [
-  {path: '', component: UsuarioComponent},
-  {path: 'form', component: UsuarioFormComponent},
+
+  {path: 'usuarios/form', component: UsuarioFormComponent},
 
 ];
 
